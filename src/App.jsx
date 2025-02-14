77b761c0911ef4fd4d9b4597edd1dde616a07ee2
mikiIDE <kinoko.miki@gmail.com>
2025-02-14 14:29:33 +0900

    save b4 WP

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
-import { useState, useEffect, useContext, useRef, useReducer } from "react"
+import { useState, useEffect, useContext, useRef, useReducer, useMemo } from "react"
 import MyInfoContext from './main';
+import SomeChild from './SomeChild';
 
 // action名称は自由に設定してOK！今回はincre/decrement
 const reducer = (state, action) => {
@@ -12,23 +13,48 @@ const reducer = (state, action) => {
 }
 
 function App() {
+
+  // useState
   const [count, setCount] = useState(0);
   const handleClick = () => {
     setCount(count+1)
   };
 
+  // useEffect
   useEffect(() => {
     console.log("Hello Hooks!")
   }, [count]);
 
+  // useContext
   const myInfo = useContext(MyInfoContext)
 
+  // useRef
   const ref = useRef()
   const handleRef =() => {
     console.log(ref.current)
   }
 
+  // useReducer
   const [state, dispatch] = useReducer(reducer, 0)
+
+  // useMemo
+  const [count01, setCount01] = useState(0);
+  const [count02, setCount02] = useState(0);
+
+  const square = useMemo(() => {
+    let i = 0;
+    while (i < 2000000){ //これが重たい処理
+      i++;
+    }
+    console.log("クリックされました")
+    return count02*count02;
+  },[count02])
+
+  // useCallBack 関数のメモ化
+  // const [counter, useCallBack] = useState(0);
+  // const showCount = useCallBack(() => {
+  //   alert("これは思い処理です。");
+  // },[counter])
 
   return (
     <>
@@ -63,6 +89,30 @@ function App() {
         <p>カウント：{state}</p>
         <button onClick={() => dispatch({ type: "increment" })}>+</button>
         <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+
+        <hr />
+
+        <h1>useMemo</h1>
+        <p>ブラウザのメモリに保存（メモ）することができる！</p>
+        <p>useStateを使った場合、カウント01を押下するだけの場合も処理が重たくなってしまう</p>
+        <p>useMemoを使えば、カウント02の時だけ重い処理が走り01はスムーズに動く</p>
+        <p>最初から使うことは殆どなく、大規模開発を進めていく中で重たくなったときにパフォーマンスチューニングを行うときに設定する</p>
+        <div>カウント1：{count01}</div>
+        <div>カウント2：{count02}</div>
+        <div>結果：{square}</div>
+        <button onClick={() => setCount01(count01 + 1)}>+</button>
+        <button onClick={() => setCount02(count02 + 1)}>+</button>
+        <p>※何でもuseMemoを使ってしまうとブラウザのメモリを圧迫する→それが原因で重たくなるため使い所を考えるべき</p>
+
+        <hr />
+
+        <h1>useCollBack</h1>
+        <p>関数のメモ化</p>
+        {/* <SomeChild show Count={showCount} /> */}
+
+        <hr />
+
+        <h1>カスタムフック</h1>
 
 
       </div>
